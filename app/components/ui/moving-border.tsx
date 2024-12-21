d343eaeb1d83a09ddc5a2851784714682c1af389
Abhinandan-4321 <abhinandangupta1105@gmail.com>
2024-12-21 19:14:41 +0530

    made changes to fix build errors

--- app/components/ui/moving-border.tsx
+++ app/components/ui/moving-border.tsx
@@ -21,7 +21,7 @@ export function MovingBorderBtn({
   borderClassName?: string;
   duration?: number;
   className?: string;
-  [key: string]: any;
+  [key: string]: unknown;
 }) {
   return (
     <Component
@@ -74,7 +74,7 @@ export const MovingBorder = ({
   duration?: number;
   rx?: string;
   ry?: string;
-  [key: string]: any; 
+  [key: string]: unknown; 
 }) => {
   const pathRef = useRef<SVGRectElement | null>(null); 
   const progress = useMotionValue<number>(0);
